88c289cb1e315d86c22c04a20c1f63a7a7c313b8
Lulu-ai322 <nazrul.islam.5.7.8.kotchandpur@gmail.com>
2026-03-04 15:29:55 +0600

    Update PlayerDataManager.java

--- main/java/me/nozrul/dragonspear/manager/PlayerDataManager.java
+++ main/java/me/nozrul/dragonspear/manager/PlayerDataManager.java
@@ -1,1 +1,38 @@
+package me.nozrul.dragonspear.manager;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import me.nozrul.dragonspear.DragonSpear;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PlayerDataManager {
+
+    private static File getFile(Player p) {
+        File folder = new File(DragonSpear.get().getDataFolder(), "playerdata");
+        if (!folder.exists()) folder.mkdirs();
+        return new File(folder, p.getUniqueId() + ".yml");
+    }
+
+    public static FileConfiguration getData(Player p) {
+        return YamlConfiguration.loadConfiguration(getFile(p));
+    }
+
+    public static int getLevel(Player p) {
+        return getData(p).getInt("level", 1);
+    }
+
+    public static int getXP(Player p) {
+        return getData(p).getInt("xp", 0);
+    }
+
+    public static void save(Player p, int level, int xp) {
+        FileConfiguration data = getData(p);
+        data.set("level", level);
+        data.set("xp", xp);
+        try { data.save(getFile(p)); }
+        catch (IOException e) { e.printStackTrace(); }
+    }
+}
