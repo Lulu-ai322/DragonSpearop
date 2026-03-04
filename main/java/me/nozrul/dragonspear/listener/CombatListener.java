e1e01c896293c2d2b47cfce2280a13526f3bca98
Lulu-ai322 <nazrul.islam.5.7.8.kotchandpur@gmail.com>
2026-03-04 15:27:25 +0600

    Update CombatListener.java

--- main/java/me/nozrul/dragonspear/listener/CombatListener.java
+++ main/java/me/nozrul/dragonspear/listener/CombatListener.java
@@ -1,1 +1,29 @@
+package me.nozrul.dragonspear.listener;
 
+import org.bukkit.event.*;
+import org.bukkit.event.entity.*;
+import org.bukkit.entity.Player;
+import me.nozrul.dragonspear.util.SpearUtil;
+import me.nozrul.dragonspear.manager.WeaknessManager;
+
+public class CombatListener implements Listener {
+
+    @EventHandler
+    public void onHit(EntityDamageByEntityEvent e) {
+
+        if (!(e.getDamager() instanceof Player)) return;
+        if (!(e.getEntity() instanceof Player)) return;
+
+        Player attacker = (Player) e.getDamager();
+        Player victim = (Player) e.getEntity();
+
+        if (!SpearUtil.isSpear(
+                attacker.getInventory().getItemInMainHand()))
+            return;
+
+        String type = SpearUtil.getType(
+                attacker.getInventory().getItemInMainHand());
+
+        WeaknessManager.apply(e, attacker, victim, type);
+    }
+}
